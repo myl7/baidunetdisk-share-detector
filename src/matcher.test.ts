44ca43d5daa3142ea9fa37b8bd3fe2849cdf13f8
myl7 <myl.ustc@gmail.com>
2020-08-21 04:42:23 +0800

    Redesign matcher and add multi match support.

--- src/matcher.test.ts
+++ src/matcher.test.ts
@@ -1,38 +1,55 @@
 import matcher from './matcher'
 
-it('regular copied share text', () => {
+it('share text match', () => {
   expect(
     matcher(
       '链接:https://pan.baidu.com/s/1cIhV2wBM4o04RGfQlijERg 提取码:1234 ' +
       '复制这段内容后打开百度网盘手机App，操作更方便哦',
     ),
-  ).toEqual({
+  ).toEqual([ {
     url: 'https://pan.baidu.com/s/1cIhV2wBM4o04RGfQlijERg',
     secret: '1234',
-  })
+  } ])
 })
 
-it('simplified share text', () => {
-  expect(matcher('s/1cIhV2wBM4o04RGfQl-_ERg 码：1234')).toEqual({
+it('simplified share text match', () => {
+  expect(matcher('s/1cIhV2wBM4o04RGfQl-_ERg 码：1234')).toEqual([ {
     url: 'https://pan.baidu.com/s/1cIhV2wBM4o04RGfQl-_ERg',
     secret: '1234',
-  })
+  } ])
 })
 
-it('weird share text', () => {
-  expect(matcher('/1cIhV2wBM4o04RGfQl-_ERg 1234')).toEqual({
+it('simplified share text with simplified secret match', () => {
+  expect(matcher('s/1cIhV2wBM4o04RGfQl-_ERg 1234')).toEqual([ {
     url: 'https://pan.baidu.com/s/1cIhV2wBM4o04RGfQl-_ERg',
     secret: '1234',
-  })
+  } ])
 })
 
-it('share text with emoji', () => {
+it('share text with emoji match', () => {
   expect(
     matcher(
-      '链接:https:// #(大笑) pan.baidu.com/s/1cIh#(幸灾乐祸)V2wBM4o #(高兴) 04RGfQlijERg 提取码:1234',
+      '链接:https:// #(大笑) pan.baidu.com/s/1cIh#(幸灾乐祸)V2wBM4o #[高兴] 04RGfQl#[高兴]ijERg 提取码:1234',
     ),
-  ).toEqual({
+  ).toEqual([ {
     url: 'https://pan.baidu.com/s/1cIhV2wBM4o04RGfQlijERg',
     secret: '1234',
-  })
+  } ])
+})
+
+it('share text multi match', () => {
+  expect(
+    matcher(
+      '链接:https://pan.baidu.com/s/1cIhV2wBM4o04RGfQlijERg 提取码:1234 ' +
+      '复制这段内容后打开百度网盘手机App，操作更方便哦！还有，' +
+      '链接:https://pan.baidu.com/s/1cIhV2wBM4o04RGfQl-_ERg 提取码:1233 ' +
+      '复制这段内容后打开百度网盘手机App，操作更方便哦',
+    ),
+  ).toEqual([ {
+    url: 'https://pan.baidu.com/s/1cIhV2wBM4o04RGfQlijERg',
+    secret: '1234',
+  }, {
+    url: 'https://pan.baidu.com/s/1cIhV2wBM4o04RGfQl-_ERg',
+    secret: '1233',
+  } ])
 })
--- src/matcher.ts
+++ src/matcher.ts
@@ -1,61 +1,51 @@
-const matcher = (text: string): {
-  url: string;
-  secret: string;
-} | null => {
-  const emojiFilters = [
-    /(\ud83c[\udf00-\udfff])|(\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55]/g,
-    / ?#\([\u4e00-\u9fa5]+\) ?/g,
-  ]
+const matcher = (text: string): { url: string, secret: string }[] => {
+  // Remove non-ascii chars.
+  text = text.replace(/[^\x00-\xff码]+/g, '')
 
-  for (let emojiFilter of emojiFilters) {
-    text = text.replace(emojiFilter, '')
+  // Remove paired punctuations.
+  text = text.replace(/(\s?)[#$%@:]?((\(\))|(\[])|({})|(<>))\1/g, '')
+
+  // Replace useless chars to spaces.
+  const cleanedText = text.replace(/[^a-zA-Z0-9-_:\/.]/g, ' ')
+
+  let urlWIthPos: { url: string, pos: number }[] = []
+
+  // Match url with pos.
+  const urlMatcher = /s\/[a-zA-Z0-9-_:\/.]+/g
+  let urlMatch
+  while ((urlMatch = urlMatcher.exec(cleanedText)) !== null) {
+    urlWIthPos.push({ url: 'https://pan.baidu.com/' + urlMatch[0], pos: urlMatcher.lastIndex })
   }
 
-  let url: string | null = null
+  let links: { url: string, secret: string }[] = []
 
-  const linkMatchers: {
-    regExp: RegExp;
-    complete: (matchedStr: string) => string;
-  }[] = [
-    {
-      regExp: /s\/[-_0-9a-zA-Z]+/,
-      complete: (matchedStr) => 'https://pan.baidu.com/' + matchedStr,
-    },
-    {
-      regExp: /\/[-_0-9a-zA-Z]+/,
-      complete: (matchedStr) => 'https://pan.baidu.com/s' + matchedStr,
-    },
-  ]
-
-  for (let linkMatcher of linkMatchers) {
-    let linkMatch = linkMatcher.regExp.exec(text)
-    if (linkMatch !== null) {
-      url = linkMatcher.complete(linkMatch[0])
-      break
-    }
-  }
-
-  if (url === null) {
-    return null
-  }
-
-  let secret: string = ''
-
-  const secretMatchers: RegExp[] = [
+  const secretMatchers = [
     /(?<=[码][:：])[0-9a-z]{4}\b/,
     /(?<=[-_0-9a-zA-Z]{16,}\s+)[0-9a-z{4}]{4}\b/,
     /\b[0-9a-z{4}]{4}\b/,
   ]
 
-  for (let secretMatcher of secretMatchers) {
-    let secretMatch = secretMatcher.exec(text)
-    if (secretMatch !== null) {
-      secret = secretMatch[0]
-      break
+  for (const [ i, info ] of urlWIthPos.entries()) {
+    // Match one secret between two url matches.
+    let secretRangeEnd = text.length
+    if (i < urlWIthPos.length - 1) {
+      secretRangeEnd = urlWIthPos[i + 1].pos
     }
+    const secretText = text.substring(info.pos, secretRangeEnd)
+
+    let secret = ''
+    let secretMatch
+    for (const secretMatcher of secretMatchers) {
+      if ((secretMatch = secretMatcher.exec(secretText)) !== null) {
+        secret = secretMatch[0]
+        break
+      }
+    }
+
+    links.push({ url: info.url, secret: secret })
   }
 
-  return { url: url, secret: secret }
+  return links
 }
 
 export default matcher
