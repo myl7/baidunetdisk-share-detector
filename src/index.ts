55785a5e4fcc2f6b5fb0f6d8f9f44bca040312a4
myl7 <myl.ustc@gmail.com>
2020-08-21 05:07:44 +0800

    Update interface.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import matcher from './matcher'
 
 chrome.contextMenus.create({
-  title: 'Try to get the share link and secret',
+  title: '探测百度云分享链接',
   contexts: [ 'selection' ],
   onclick: (info, _tab) => {
-    let baiduNetdiskInfo = matcher(info.selectionText as string)
-    if (baiduNetdiskInfo !== null) {
-      chrome.tabs.create({ url: baiduNetdiskInfo.url + '#' + baiduNetdiskInfo.secret })
-    } else {
+    let links = matcher(info.selectionText as string)
+
+    if (links.length == 0) {
       chrome.notifications.create({
         type: 'basic',
         iconUrl: '/icon_60x60.png',
-        title: 'Failed to detect share links',
-        message: '',
+        title: '探测失败',
+        message: '未探测到百度云分享链接！',
       })
+    } else {
+      for (const link of links) {
+        chrome.tabs.create({ url: link.url + '#secret=' + link.secret })
+      }
     }
   },
 })
